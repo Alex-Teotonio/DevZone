d30634c30af88a195c09155b12c08f35d52b5af9
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2023-04-30 13:54:22 -0300

    Iniatilizing application

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+export function Header() {
+ 
+  return(
+    <>
+    <header className={styles.header} >
+      <h1>DevZone</h1>
+    </header>
+    </>
+    
+  )
+}
